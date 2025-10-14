5914b3bed77dfb39c2f061cefce09758ef7988f9
Artur Gentil Silva Pinto <arturpinto2005@gmail.com>
2025-10-14 13:31:46 +0100

    .

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+561b7bd3efdc7bc9da23dd120dad838f9c9f9054")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fed18eeed447461480d5d905cb6cbf9c71254b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
