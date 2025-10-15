afc909f5aff9d520559b5e147261144316d0649e
Willkie <8230127@estg.ipp.pt>
2025-10-15 20:28:34 +0100

    TrashCommit Pt.2
    Cache e definições que não sei o motivo de não terem sido impedidos pelo gitignore

--- Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5914b3bed77dfb39c2f061cefce09758ef7988f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31df532bb4cacd8efff86cd410d1358d1fe2d6f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
