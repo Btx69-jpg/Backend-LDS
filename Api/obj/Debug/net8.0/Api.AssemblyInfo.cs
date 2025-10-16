9ae719133a620439f62f4a360f9a318ae9e7d670
Willkie <8230127@estg.ipp.pt>
2025-10-16 18:07:28 +0100

    features
    Comecei a desenvolver uma nova feature e percebi que estava na branch errada

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f403af0dbfa1f92a602b2273f664be9f370fdca0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a84eb5a65d8d136c103b3f49f9f57d28a6f92d65")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Application/Interfaces/ITeamRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    internal interface ITeamRepository
+    {
+        Task<IEnumerable<string>> GetAll();
+        Task<IEnumerable<string>> GetAllAsync();
+        Task<IEnumerable<Teams>> GetTeamByID(string  teamID);
+
+    }
+}
