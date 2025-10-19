698b483db725aa9211837201d20279a9c785c5c5
8230148 <8230148@estg.ipp.pt>
2025-10-19 12:17:01 +0100

    Imp. do DTO CreateUserDTO

---
+++ Application/DTOs/CreateUserDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    internal class CreateUserDTO
+    {
+        public string Name { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+}
