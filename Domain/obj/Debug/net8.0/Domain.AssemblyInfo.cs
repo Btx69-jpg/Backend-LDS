c95425e914f604da39ff5c77ee32c234564fd278
8230148 <8230148@estg.ipp.pt>
2025-10-19 11:22:46 +0100

    teste gitignore

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a84eb5a65d8d136c103b3f49f9f57d28a6f92d65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57c09b37378ba7bae7c3728daaf6f3f37c11b0fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
