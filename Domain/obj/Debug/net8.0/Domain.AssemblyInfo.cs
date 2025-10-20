faf76f16d9acd13ae4e2d6dedef0c09e982ab73a
Willkie <8230127@estg.ipp.pt>
2025-10-20 21:23:29 +0100

    add yml

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37d6b551f82fa86268d14c806380bd84ee4d5629")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08f2f118ab887d69f74b881ffbd7b975b6c211c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
