ff03d131ee18775222003a6c5ea6cbd2d7ad9b52
8230148 <8230148@estg.ipp.pt>
2025-10-28 10:52:49 +0000

    features de conta de jogador que não ficaram

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65119df6f8f54e29aa248c42b2b0a857079d0ec1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee697584d7728e7deb2f2c6ed55732c8dd3fba4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
