b728e0744f5886a507074ab82026a7de9a1a0f09
8230148 <8230148@estg.ipp.pt>
2025-10-29 00:51:46 +0000

    pequena correção devido a um conflito ao fazer o último merge

--- Tests/Unit/ApplicationTests/ServicesTests/TeamServiceTests.cs
+++ Tests/Unit/ApplicationTests/ServicesTests/TeamServiceTests.cs
@@ -35,6 +35,8 @@ namespace Unit.ApplicationTests.ServicesTests
         private Mock<IRankRepository> _rankRepoMock;
         private ITeamValidator _validatorReal;
         private TeamService _sut;
+        private readonly Mock<IMembershipRequestRepository> _membershipRequestRepoMock = new();
+        private readonly Mock<IPlayerValidator> _playerValidatorMock = new();
 
         /// <summary>
         /// Classe auxiliar interna para criar instâncias de Rank em contexto de teste.
@@ -79,12 +81,14 @@ namespace Unit.ApplicationTests.ServicesTests
             _validatorReal = new TeamValidator();
 
             _sut = new TeamService(
-                _teamRepoMock.Object,
-                _playerRepoMock.Object,
-                _userRepoMock.Object,
-                _unitOfWorkMock.Object,
-                _validatorReal,
-                _rankRepoMock.Object
+            _teamRepoMock.Object,
+            _playerRepoMock.Object,
+            _userRepoMock.Object,
+            _unitOfWorkMock.Object,
+            _validatorReal,
+            _rankRepoMock.Object,
+            _membershipRequestRepoMock.Object,
+            _playerValidatorMock.Object
             );
         }
 
