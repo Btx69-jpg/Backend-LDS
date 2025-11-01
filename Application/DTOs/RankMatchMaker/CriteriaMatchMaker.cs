d6b20d09d4fb54a56edf5775ac0022e3021316d9
Artur Gentil Silva Pinto <arturpinto2005@gmail.com>
2025-11-01 13:15:38 +0000

    Update CriteriaMatchMaker.cs

--- Application/DTOs/RankMatchMaker/CriteriaMatchMaker.cs
+++ Application/DTOs/RankMatchMaker/CriteriaMatchMaker.cs
@@ -1,13 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Domain.Constants;
+using System.ComponentModel.DataAnnotations;
 
 namespace Application.DTOs.RankMatchMaker
 {
     public class CriteriaMatchMaker
     {
         [Required]
+        [Range(ModelConstants.DeafultCriteriaMatchMaker.differencePoint, ModelConstants.DeafultCriteriaMatchMaker.maxDifferencePoint)]
         public int differencPoints { get; set; }
 
         [Required]
+        [Range(ModelConstants.DeafultCriteriaMatchMaker.differenceAverageAge, ModelConstants.DeafultCriteriaMatchMaker.maxDifferenceAverageAge)]
         public float diffAverageAge { get; set; }
     }
 }
