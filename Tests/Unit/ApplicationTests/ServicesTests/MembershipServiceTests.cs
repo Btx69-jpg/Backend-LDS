095a6722ed38487b8e0b94f6210a35d409ba126f
IanDCosta <8220005@estg.ipp.pt>
2025-11-01 17:00:34 +0000

    membershitservicetests

---
+++ Tests/Unit/ApplicationTests/ServicesTests/MembershipServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using Application.Interfaces.Repositories;
+using Application.Services;
+using Domain.Entities;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Unit.ApplicationTests.ServicesTests
+{
+    [TestFixture]
+    public class MembershipServiceTests
+    {
+        private Mock<IPlayerRepository> _playerRepositoryMock;
+        private Mock<ITeamRepository> _teamRepositoryMock;
+        private Mock<IMembershipRequestRepository> _membershipRepositoryMock;
+        private Mock<IUnityOfWork> _unityOfWorkMock;
+        private MembershipService _membershipService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _playerRepositoryMock = new Mock<IPlayerRepository>();
+            _teamRepositoryMock = new Mock<ITeamRepository>();
+            _membershipRepositoryMock = new Mock<IMembershipRequestRepository>();
+            _unityOfWorkMock = new Mock<IUnityOfWork>();
+
+            _membershipService = new MembershipService(
+                _playerRepositoryMock.Object,
+                _teamRepositoryMock.Object,
+                _membershipRepositoryMock.Object,
+                _unityOfWorkMock.Object
+                );
+        }
+    }
+}
