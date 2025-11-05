f9a30887266780b225005dda394dff562714102d
Artur Gentil Silva Pinto <arturpinto2005@gmail.com>
2025-11-05 09:27:23 +0000

    .

--- Application/Services/PlayerService.cs
+++ Application/Services/PlayerService.cs
@@ -1,12 +1,10 @@
 ﻿using Application.DTOs.Filters;
-using Application.DTOs.MemberShip;
 using Application.DTOs.PlayerDTOs;
 using Application.DTOs.Team;
 using Application.Interfaces.Repositories;
 using Application.Interfaces.Services;
 using Application.Interfaces.Validators;
 using Domain.Entities;
-using System.ComponentModel.DataAnnotations;
 
 namespace Application.Services
 {
@@ -161,131 +159,6 @@ namespace Application.Services
 
             return teamName;
         }
-        #endregion
-
-        #region Membership Requests
-
-        public async Task<List<MemberShipRequestDto>> GetMembershipRequestsAsync(string playerId)
-        {
-            var player = await playerRepository.GetPlayerByIdAsync(playerId);
-            authorizationValidator.ValidatePlayerAutorizationWithoutTeam(player);
-
-            return await playerRepository.GetMembershipRequestsDtoAsync(playerId);
-        }
-
-        public async Task<List<MemberShipRequestDto>> GetMembershipRequestsAsyncWithFilters(string playerId, FilterMembershipRequestsPlayer filters)
-        {
-            var player = await playerRepository.GetPlayerByIdAsync(playerId);
-            authorizationValidator.ValidatePlayerAutorizationWithoutTeam(player);
-
-            return await playerRepository.GetMembershipRequestsDtoAsyncWithFilters(playerId, filters);
-        }
-
-        public async Task<MemberShipRequestDto> AcceptMembershipRequestAsync(string playerId, Guid requestId)
-        {
-            var player = await playerRepository.GetPlayerByIdWithRequestsAsync(playerId)
-                ?? throw new ValidationException($"O jogador com Id '{playerId}' não existe.");
-
-            authorizationValidator.ValidatePlayerAutorizationWithoutTeam(player);
-
-            var request = player.MembershipRequests?.FirstOrDefault(r => r.Id == requestId)
-                ?? throw new ValidationException($"O jogador não possui um pedido de adesão com Id '{requestId}'.");
-
-            var team = await teamRepository.GetTeamForMembershipRequestAsync(request.IdTeam)
-                ?? throw new ValidationException($"A equipa com Id '{request.IdTeam}' não existe.");
-
-            player.IdTeam = team.Id;
-            team.Members.Add(player);
-
-            player.MembershipRequests.Remove(request);
-            team.MembershipRequests?.Remove(request);
-
-            await unityOfWork.SaveChangesAsync();
-
-            var fullPlayer = await playerRepository.GetPlayerByIdAsync(request.IdPlayer);
-            var fullTeam = await teamRepository.GetTeamByIdAsync(request.IdTeam);
-
-            return new MemberShipRequestDto
-            {
-                RequestId = request.Id,
-                PlayerId = fullPlayer.Id,
-                PlayerName = fullPlayer.Name,
-                TeamId = fullTeam.Id,
-                TeamName = fullTeam.Name,
-                RequestDate = request.InviteDate,
-                IsPlayerSender = request.IsPlayerSender
-            };
-        }
-
-        public async Task<MemberShipRequestDto> RejectMembershipRequestAsync(string playerId, Guid requestId)
-        {
-            var player = await playerRepository.GetPlayerByIdWithRequestsAsync(playerId)
-                ?? throw new ValidationException($"O jogador com Id '{playerId}' não existe.");
-
-            authorizationValidator.ValidatePlayerAutorizationWithoutTeam(player);
-
-            var request = player.MembershipRequests?.FirstOrDefault(r => r.Id == requestId)
-                ?? throw new ValidationException($"O jogador não possui um pedido de adesão com Id '{requestId}'.");
-
-            var fullPlayer = await playerRepository.GetPlayerByIdAsync(request.IdPlayer);
-            var fullTeam = await teamRepository.GetTeamByIdAsync(request.IdTeam);
-
-            player.MembershipRequests.Remove(request);
-
-            await unityOfWork.SaveChangesAsync();
-
-            return new MemberShipRequestDto
-            {
-                RequestId = request.Id,
-                PlayerId = fullPlayer.Id,
-                PlayerName = fullPlayer.Name,
-                TeamId = fullTeam.Id,
-                TeamName = fullTeam.Name,
-                RequestDate = request.InviteDate,
-                IsPlayerSender = request.IsPlayerSender
-            };
-        }
-
-        //faz validation de se o player ja tem equipa
-        public async Task<MemberShipRequestDto> SendMembershipRequestAsync(string playerId, Guid teamId)
-        {
-            var player = await playerRepository.GetPlayerByIdAsync(playerId);
-
-            authorizationValidator.ValidatePlayerAutorizationWithoutTeam(player);
-
-            var team = await teamRepository.GetTeamForMembershipRequestAsync(teamId);
-
-            var existingRequest = await membershipRequestRepository
-                .GetMembershipRequestByPlayerAndTeam(playerId, teamId);
-
-            playerValidator.SendMembershipRequestValidator(player, team, existingRequest);
-
-            var newRequest = new MembershipRequest
-            {
-                Id = Guid.NewGuid(),
-                IdPlayer = playerId,
-                IdTeam = teamId,
-                Player = player,
-                Team = team,
-                InviteDate = DateTime.UtcNow,
-                IsPlayerSender = true
-            };
-
-            await membershipRequestRepository.AddMembershipRequest(newRequest);
-            await unityOfWork.SaveChangesAsync();
-
-            return new MemberShipRequestDto
-            {
-                RequestId = newRequest.Id,
-                PlayerId = newRequest.IdPlayer,
-                PlayerName = newRequest.Player.Name,
-                TeamId = newRequest.IdTeam,
-                TeamName = newRequest.Team.Name,
-                RequestDate = newRequest.InviteDate,
-                IsPlayerSender = newRequest.IsPlayerSender
-            };
-        }
-
         #endregion
 
         #region Lists
