fcd1ef68a28c9d932a9ffa4e99d9368e0ef92695
Willkie <8230127@estg.ipp.pt>
2025-11-07 15:15:51 +0000

    db fix

--- Infrastructure/Migrations/20251107151503_phoneFix.Designer.cs
+++ Infrastructure/Migrations/20251107151503_phoneFix.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(AmateurFootballContext))]
-    partial class AmateurFootballContextModelSnapshot : ModelSnapshot
+    [Migration("20251107151503_phoneFix")]
+    partial class phoneFix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
