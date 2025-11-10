4b246005d5384aaed52ce06cdf664c42faaa8f2e
IanDCosta <8220005@estg.ipp.pt>
2025-11-10 20:36:12 +0000

    player integration tests

--- Tests/Integration/ApiTestAppFactory.cs
+++ Tests/Integration/ApiTestAppFactory.cs
@@ -1,4 +1,6 @@
-﻿using Application.Interfaces.Validators.Hub;
+﻿using Application.Interfaces.Services;
+using Application.Interfaces.Validators.Hub;
+using Google.Cloud.Firestore;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Hosting;
@@ -15,8 +17,6 @@ namespace Tests.Integration
 {
     public class ApiTestAppFactory : WebApplicationFactory<Program>
     {
-        public string TestUserId { get; private set; } = "fake-user-id-" + Guid.NewGuid();
-
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.ConfigureServices(services =>
@@ -45,6 +45,10 @@ namespace Tests.Integration
                     services.Remove(h);
                 }
 
+                RemoveFirebaseServices(services);
+
+                AddFirebaseServiceMocks(services);
+
                 services.AddDbContext<AmateurFootballContext>(options =>
                 {
                     options.UseInMemoryDatabase("InMemoryDbForTesting");
@@ -69,6 +73,66 @@ namespace Tests.Integration
                 services.AddSingleton<IHubFinshMatchValidator>(mockHubFinshValidator.Object);
                 services.AddSingleton<IGeralHubValidator>(mockGeralValidator.Object);
             });
+        }
+
+        private void RemoveFirebaseServices(IServiceCollection services)
+        {
+            services.RemoveAll<FirestoreDb>();
+
+            var firebaseServices = services
+                .Where(s =>
+                    (s.ServiceType.Namespace != null && (
+                        s.ServiceType.Namespace.Contains("Firebase") ||
+                        s.ServiceType.Namespace.Contains("Google.Cloud") ||
+                        s.ServiceType.Namespace.Contains("Firestore"))) ||
+                    (s.ImplementationType?.Namespace?.Contains("Firebase") == true) ||
+                    (s.ImplementationType?.Namespace?.Contains("Google.Cloud") == true) ||
+                    (s.ImplementationFactory?.Method.ReturnType.Namespace?.Contains("Firebase") == true) ||
+                    (s.ImplementationFactory?.Method.ReturnType.Namespace?.Contains("Google.Cloud") == true))
+                .ToList();
+
+            foreach (var service in firebaseServices)
+            {
+                services.Remove(service);
+            }
+
+            var specificFirebaseServices = new List<Type>
+            {
+                typeof(IChatRoomService),
+                typeof(IAuthService),
+                typeof(ISuperAdminService)
+            };
+
+            foreach (var serviceType in specificFirebaseServices)
+            {
+                var descriptors = services.Where(d => d.ServiceType == serviceType).ToList();
+                foreach (var descriptor in descriptors)
+                {
+                    services.Remove(descriptor);
+                }
+            }
+        }
+
+        private void AddFirebaseServiceMocks(IServiceCollection services)
+        {
+            var mockAuthService = new Mock<IAuthService>();
+            mockAuthService.Setup(x => x.RegisterUser(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(Guid.NewGuid().ToString());
+            mockAuthService.Setup(x => x.LoginAsync(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(new Application.DTOs.LoginResponseDto());
+            mockAuthService.Setup(x => x.DeleteUserAsync(It.IsAny<string>())).Verifiable();
+                
+            mockAuthService.Setup(x => x.UpdateEmailAsync(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Task.CompletedTask);
+            
+
+            services.AddSingleton(mockAuthService.Object);
+
+            var mockChatService = new Mock<IChatRoomService>();
+            services.AddSingleton(mockChatService.Object);
+
+            var mockSuperAdminService = new Mock<ISuperAdminService>();
+            services.AddSingleton(mockSuperAdminService.Object);
         }
 
         public void SeedDatabase(Action<AmateurFootballContext> seeder)
